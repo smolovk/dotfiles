8f9cea629b01b6f9fa4984c761b5959c420026c6
smolovk <smolov326@gmail.com>
2024-01-29 21:33:45 +0200

    lsp comp fix

--- nvim/.config/nvim/lua/lspzero.lua
+++ nvim/.config/nvim/lua/lspzero.lua
@@ -22,3 +22,21 @@ require('mason-lspconfig').setup({
     lsp_zero.default_setup,
   },
 })
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<cr>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  mapping = cmp_mappings
+})
+
+
